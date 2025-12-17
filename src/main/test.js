1c0ad08ea7983466506fadffc0fb2edb9ec23d28
parkjay0709 <soRjdla0709@gmail.com>
2025-12-17 09:52:15 +0900

    feat : js와 C++ 연동

---
+++ src/main/test.js
@@ -0,0 +1,27 @@
+const { spawn } = require("child_process");
+
+const cpp = spawn(
+  "src\\main\\main.exe"
+);
+
+cpp.stdout.on("data", data => {
+  console.log("C++:", data.toString());
+});
+
+cpp.stderr.on("data", data => {
+  console.error("C++ ERR:", data.toString());
+});
+
+cpp.on("error", err => {
+  console.error("실행 실패:", err.message);
+});
+
+const spellData = {
+  spellId: "SPELL_001",
+  pronunciation: 87.3,
+  volume: 60,
+  emotion: 75
+};
+
+// 반드시 개행 포함
+cpp.stdin.write(JSON.stringify(spellData) + "\n");
