8bc6bf484481c29d5ae001068b2fe27645fb13cb
parkjay0709 <soRjdla0709@gmail.com>
2025-12-17 16:20:48 +0900

    ...

---
+++ src/core/Character.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Character
+{
+    public :
+        std::string name;
+        int hp;
+        int attack;
+        int strongAttack;
+
+    public :
+        Character( const std::string& name, int hp, int attack, int strongAttack );
+
+        void Attack( Character& target );
+        void StrongAttack( Character& target );
+        void takeDamage( int dmg );
+        bool isDead() const;
+
+        void PrintStatus();
+};
