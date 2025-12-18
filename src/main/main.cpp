91ad31bbf276eb9d32bdad036b2f3fdc7ef57214
parkjay0709 <soRjdla0709@gmail.com>
2025-12-18 11:39:29 +0900

    json 처리 최종

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,34 +1,7 @@
-#include <iostream>
-
 #include "../game/GameManager.h"
 
 int main()
 {
-
-    // if( pronunciation >= 70 )
-    // {
-    //     cout << "성공" << endl;
-    //     if( volume > 85 )
-    //     {
-    //         cout << "공격 실패" << endl;
-    //     } // if
-    //     else if( volume >= 70 )
-    //     {
-    //         cout << "강공격" << endl;
-    //     } // else if
-    //     else if( volume >= 40 )
-    //     {
-    //         cout << "기본 공격력" << endl;
-    //     } // else if
-    //     else
-    //     {
-    //         cout << "실패" << endl;
-    //     } // else
-    // } // if
-    // else
-    // {
-    //     cout << "실패" << endl;
-    // } // else
     GameManager game;
     game.Run();
 
